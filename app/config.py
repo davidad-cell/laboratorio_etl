3cbd5c7e4dd98f19424cf61bfaa46adc0511a4d7
David Alzate <davidalzatediaz39@gmail.com>
2026-02-02 22:56:47 -0500

    Organizamos archivo config

--- app/config.py
+++ app/config.py
@@ -1,36 +1,22 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from app.controllers.etl_controller import router as etl_router
-from app.database import get_mysql_engine, Base
+from dotenv import load_dotenv
+import os
 
-def create_app() -> FastAPI:
-    app = FastAPI(
-        title="ETL Pipeline API - Disney",
-        description="Laboratorio Final - Pipeline ETL con FastAPI, MongoDB y MySQL usando Disney API",
-        version="1.0.0"
-    )
+# Cargar variables de entorno desde .env
+load_dotenv()
 
-    # Incluir rutas del controlador ETL con prefijo
-    app.include_router(etl_router, prefix="/api/v1/etl", tags=["ETL"])
 
-    @app.on_event("startup")
-    async def on_startup() -> None:
-        try:
-            engine = get_mysql_engine()
-            Base.metadata.create_all(bind=engine)
-            with engine.connect() as conn:
-                conn.execute(text("SELECT 1"))
-                conn.commit()
-            print("Conexion iniciada")
-        except Exception as e:
-            print(f"Error en conexión : {e}")
+class config:
+    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_DB = os.getenv("MONGO_DB", "disney_staging")
 
     
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_DB = os.getenv("MYSQL_DB")
 
-    @app.get("/health")
-    async def health_check():
-        return {"status": "healthy", "service": "etl-pipeline"}
-
-    return app
-
-app = create_app()
+    
+    DISNEY_API_URL = os.getenv(
+        "DISNEY_API_URL",
+        "https://api.disneyapi.dev/character"
+    )
